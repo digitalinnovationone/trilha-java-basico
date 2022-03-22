4169ba2f213d00386de378b9cfa92bd53a8c29a2
Gleyson Sampaio <glysns@hotmail.com>
2022-03-22 18:19:32 -0300

    finalizando operadores

--- codigo fonte/tipos-variaveis/src/Operadores.java
+++ codigo fonte/tipos-variaveis/src/Operadores.java
@@ -1,7 +1,5 @@
 public class Operadores {
     public static void main(String[] args) {
-       int numero = 18;
-       int modulo = 3;
-       System.out.println(numero % modulo);
+        
     }
 }
