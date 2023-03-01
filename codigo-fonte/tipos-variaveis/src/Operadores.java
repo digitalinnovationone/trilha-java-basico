2e6e1b8bbdf6f692b04959d247153bf22e593d88
Allana Larissa <larissaleonel98@gmail.com>
2023-03-01 15:58:09 -0300

    first commit

--- codigo-fonte/tipos-variaveis/src/Operadores.java
+++ codigo-fonte/tipos-variaveis/src/Operadores.java
@@ -1,5 +1,10 @@
 public class Operadores {
     public static void main(String[] args) {
-        
+        String meuNome = "lari";
+        int nasc = 2022;
+        boolean verdadeira = true;
+        nasc = 1998;
+
+
     }
 }
