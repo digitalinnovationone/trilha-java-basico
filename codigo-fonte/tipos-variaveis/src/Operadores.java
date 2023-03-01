c0a4a3f2b3168e63ccd7aa3050a934b101438451
Allana Larissa <larissaleonel98@gmail.com>
2023-03-01 16:09:53 -0300

    second commit

--- codigo-fonte/tipos-variaveis/src/Operadores.java
+++ codigo-fonte/tipos-variaveis/src/Operadores.java
@@ -1,9 +1,5 @@
 public class Operadores {
     public static void main(String[] args) {
-        String meuNome = "lari";
-        int nasc = 2022;
-        boolean verdadeira = true;
-        nasc = 1998;
 
 
     }
---
+++ codigo-fonte/tipos-variaveis/src/TiposClasses.java
@@ -0,0 +1,21 @@
+public class TiposClasses {
+    public static void main(String[] args) {
+        String meuNome = "lari";
+        int nasc = 2022;
+        boolean verdadeira = true;
+        nasc = 1998;
+        String primeiroNome = "larissa";
+        String segundoNome = "leonel";
+        String nomeCompleto = nomeCompleto(primeiroNome,segundoNome);
+        System.out.println(nomeCompleto);
+
+
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return  primeiroNome.concat(" ").concat(segundoNome);
+    }
+
+
+}
+
+
