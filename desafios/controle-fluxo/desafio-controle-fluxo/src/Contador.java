5fb2f7c1221e6ebe57d58c3c160bd96d335e2d0c
GleidsonLM <gleidsonlm@gmail.com>
2023-08-24 17:46:10 -0300

    feat(gleidsonlm): flow control challenge solved

---
+++ desafios/controle-fluxo/desafio-controle-fluxo/src/Contador.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Contador {
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+
+		try {
+			// chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+
+		} catch (ParametrosInvalidosException exception) {
+			// imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+			System.out.println(exception);
+		}
+
+		terminal.close();
+
+	}
+
+	static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+		// validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		if (parametroUm > parametroDois) {
+			throw new ParametrosInvalidosException();
+		}
+
+		int contagem = parametroDois - parametroUm;
+		// realizar o for para imprimir os números com base na variável contagem
+		for (int i = 0; i <= contagem; i++) {
+			System.out.println("Imprimindo o número " + (parametroUm + i));
+		}
+	}
+}
---
+++ desafios/controle-fluxo/desafio-controle-fluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException() {
+        super("O segundo parâmetro deve ser maior que o primeiro");
+    }
+}
