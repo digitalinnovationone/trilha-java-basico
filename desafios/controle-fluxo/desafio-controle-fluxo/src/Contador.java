6ad9ab74c81a7a1d432aa6b7cd5146d2d266c857
GleidsonLM <gleidsonlm@gmail.com>
2023-08-24 17:58:11 -0300

    docs: class and functions javadocs comments

--- desafios/controle-fluxo/desafio-controle-fluxo/src/Contador.java
+++ desafios/controle-fluxo/desafio-controle-fluxo/src/Contador.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
 
+/**
+ * Counts from a number to another number given by the user in the terminal.
+ * 
+ * @author GleidsonLM
+ * @version 0.1
+ * @since 2023-08-24
+ */
 public class Contador {
+	/**
+	 * Receives user input and calls the method that contains the logic to count.
+	 * 
+	 * @param args
+	 * @see #contar(int, int)
+	 */
 	public static void main(String[] args) {
 		Scanner terminal = new Scanner(System.in);
 
@@ -23,6 +36,14 @@ public class Contador {
 
 	}
 
+	/**
+	 * Receives two numbers, and prints the numbers between then in the terminal.
+	 * 
+	 * @param parametroUm
+	 * @param parametroDois
+	 * @throws ParametrosInvalidosException
+	 * 
+	 */
 	static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
 		// validar se parametroUm é MAIOR que parametroDois e lançar a exceção
 		if (parametroUm > parametroDois) {
