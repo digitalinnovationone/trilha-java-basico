489aedc2f6a4b16a3b2552ff47066c92086bef2e
layokaminski <layokaminski@gmail.com>
2023-10-12 12:47:22 -0300

    refactor: Implementa UML do iphone

---
+++ codigo-fonte/tipos-variaveis/src/service/impl/Iphone.java
@@ -0,0 +1,52 @@
+package service.impl;
+
+import service.InternetBrowser;
+import service.MusicPlayer;
+import service.Telephone;
+
+public class Iphone implements MusicPlayer, Telephone, InternetBrowser {
+    @Override
+    public void showPage() {
+
+    }
+
+    @Override
+    public void addNewTab() {
+
+    }
+
+    @Override
+    public void updatePage() {
+
+    }
+
+    @Override
+    public void touchMusic(String music) {
+
+    }
+
+    @Override
+    public void pauseMusic() {
+
+    }
+
+    @Override
+    public void selectMusic(String music) {
+
+    }
+
+    @Override
+    public void call() {
+
+    }
+
+    @Override
+    public void answer() {
+
+    }
+
+    @Override
+    public void startVoiceMail(String voiceMail) {
+
+    }
+}
