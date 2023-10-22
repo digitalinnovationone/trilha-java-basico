5d8b2e9fed3ba6724d8d31f045fa152edd4252ca
brunofbdias <brunofbdias@gmail.com>
2023-10-22 15:48:52 -0300

    Enviando projeto de controle de fluxo para o fork do Github

---
+++ desafios/controle-fluxo/DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args, String string) throws Exception {
+
+            Scanner terminal = new Scanner(System.in);
+
+            System.out.println("Digite o primeiro parâmetro:");
+            int parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parâmetro:");
+            int parametroDois = terminal.nextInt();
+            terminal.close();
+            
+            try {
+                //chamando o método contendo a lógica de contagem
+                contar(parametroUm, parametroDois);
+            
+            }catch (ParametrosInvalidosException e) {
+                //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+                System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+            }
+            
+    }
+        static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+            //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+            if(parametroUm > parametroDois){
+                throw new ParametrosInvalidosException();
+            }
+            int contagem = parametroDois - parametroUm;
+            //realizar o for para imprimir os números com base na variável contagem
+
+            for (int i = contagem; i < parametroDois; i++){
+                System.out.println("Imprimindo o número " + i);
+            }
+        };
+    }
+
