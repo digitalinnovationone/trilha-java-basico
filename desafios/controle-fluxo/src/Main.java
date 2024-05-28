c9721d81ac3f351855000de41ba92c9b220f370f
Tallia-Sousa <talliasousa.com@gmail.com>
2024-05-28 01:29:09 -0300

    Adicionando Desafio controle de fluxo

---
+++ desafios/controle-fluxo/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            //chamando o método contendo a lógica de contagem
+            contar(parametroUm, parametroDois);
+
+        }catch ( ParametrosInvalidosException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+
+
+        if(parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+        int contagem = parametroDois - parametroUm;
+        for(int i = 1; i <= contagem; i++) {
+
+            System.out.println("Imprimindo o número " +i);
+        }
+        //realizar o for para imprimir os números com base na variável contagem
+    }
+}
+
+
+
---
+++ desafios/controle-fluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+public class ParametrosInvalidosException extends Exception {
+
+    public ParametrosInvalidosException(String mensagem){
+        super(mensagem);
+    }
+
+}
