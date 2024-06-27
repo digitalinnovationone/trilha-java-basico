3fe7ce69d649f8b8f80cd511a6d26a28cea61771
Lucas Job Viana <lucasjobvianna@gmail.com>
2024-06-27 19:40:20 -0300

    Adicionado desafio controle-fluxo

---
+++ desafios/controle-fluxo/ParametrosInvalidosException.java
@@ -0,0 +1,5 @@
+public class ParametrosInvalidosException extends RuntimeException {
+    public ParametrosInvalidosException(){
+        super("O segundo parâmetro deve ser maior que o primeiro");
+    }
+}
